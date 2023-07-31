26fef1eeff8c4775865c2be3f3d109039a511a3c
PC-AMD <PC-AMD@DESKTOP-KUILKS6>
2023-07-31 13:03:37 -0300

    1er commit

---
+++ src/main/java/com/gonzalo/virtualwallet/VirutalWalletApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalo.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirutalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirutalWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzalo/virtualwallet/VirutalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalo.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirutalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
